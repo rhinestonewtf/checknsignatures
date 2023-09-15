27e41ceec81eeed3bc62f14560b801f0a5df3ac2
zeroknots <zeroknots@protonmail.com>
2023-09-15 12:06:57 +0700

    adding tests for multisig

--- src/CheckNSignaturesFoundryHelper.sol
+++ src/CheckNSignaturesFoundryHelper.sol
@@ -3,9 +3,15 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 
+address constant VM_ADDR = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
+
+function vmsign(uint256 pk, bytes32 digest) pure returns (uint8 v, bytes32 r, bytes32 s) {
+    return Vm(VM_ADDR).sign(pk, digest);
+}
+
 /// @title CheckNSignaturesFoundryHelper
 /// @author zeroknots
-contract CheckNSignaturesFoundryHelper is Test {
+contract CheckNSignaturesFoundryHelper {
     function sign(
         uint256 signerPrivKey,
         bytes memory packedData
@@ -15,7 +21,7 @@ contract CheckNSignaturesFoundryHelper is Test {
         returns (bytes memory signature)
     {
         bytes32 dataHash = keccak256(packedData);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPrivKey, dataHash);
+        (uint8 v, bytes32 r, bytes32 s) = vmsign(signerPrivKey, dataHash);
 
         signature = abi.encodePacked(r, s, v);
     }
@@ -36,7 +42,7 @@ contract CheckNSignaturesFoundryHelper is Test {
 
         for (uint256 i; i < signerPrivKeys.length; i++) {
             uint256 privKey = signerPrivKeys[i];
-            (v, r, s) = vm.sign(privKey, dataHash);
+            (v, r, s) = vmsign(privKey, dataHash);
             signatures = abi.encodePacked(signatures, abi.encodePacked(r, s, v));
         }
     }
--- test/CheckNSignatures.t.sol
+++ test/CheckNSignatures.t.sol
@@ -6,10 +6,11 @@ import "forge-std/Test.sol";
 import "../src/CheckNSignatures.sol";
 
 import { SignatureCheckerLib } from "solady/src/utils/SignatureCheckerLib.sol";
+import { CheckNSignaturesFoundryHelper } from "../src/CheckNSignaturesFoundryHelper.sol";
 /// @title CheckNSignaturesTest
 /// @author zeroknots
 
-contract CheckNSignaturesTest is Test {
+contract CheckNSignaturesTest is Test, CheckNSignaturesFoundryHelper {
     function setUp() public { }
 
     function testCheckSignature() public {
@@ -39,5 +40,52 @@ contract CheckNSignaturesTest is Test {
 
         assertEq(signer1, recovered[0]);
         assertEq(signer2, recovered[1]);
+    }
+
+    function test_WithHelper(uint256 privKey, bytes memory data) public {
+        vm.assume(privKey < 2 ** 18);
+        vm.assume(privKey > 0);
+        bytes memory signature = sign(privKey, data);
+
+        bytes32 dataHash = keccak256(data);
+
+        address[] memory recovered = CheckSignatures.recoverNSignatures(dataHash, signature, 1);
+
+        address signer = vm.addr(privKey);
+
+        assertEq(signer, recovered[0]);
+    }
+
+    function test_WithHelper(
+        uint256 privKey1,
+        uint256 privKey2,
+        uint256 privKey3,
+        bytes memory data
+    )
+        public
+    {
+        vm.assume(privKey1 < 2 ** 18);
+        vm.assume(privKey1 > 0);
+
+        vm.assume(privKey2 < 2 ** 18);
+        vm.assume(privKey2 > 0);
+
+        vm.assume(privKey3 < 2 ** 18);
+        vm.assume(privKey3 > 0);
+        uint256[] memory privKeys = new uint256[](3);
+        privKeys[0] = privKey1;
+        privKeys[1] = privKey2;
+        privKeys[2] = privKey3;
+        bytes memory signature = sign(privKeys, data);
+
+        bytes32 dataHash = keccak256(data);
+
+        address[] memory recovered =
+            CheckSignatures.recoverNSignatures(dataHash, signature, privKeys.length);
+
+        for (uint256 i; i < privKeys.length; i++) {
+            address signer = vm.addr(privKeys[i]);
+            assertEq(signer, recovered[i]);
+        }
     }
 }
