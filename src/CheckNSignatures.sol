42acf185958afc20aa64e9217fb04af2039e0bb5
kopy-kat <kopp.konrad@gmail.com>
2024-05-21 11:37:51 +0100

    chore: natspec fix

--- src/CheckNSignatures.sol
+++ src/CheckNSignatures.sol
@@ -94,12 +94,10 @@ library CheckSignatures {
     /**
      * @notice Splits signature bytes into `uint8 v, bytes32 r, bytes32 s`.
      * @dev Make sure to perform a bounds check for @param pos, to avoid out of bounds access on
-     * @param signatures
-     *      The signature format is a compact form of {bytes32 r}{bytes32 s}{uint8 v}
-     *      Compact means uint8 is not padded to 32 bytes.
-     * @param pos Which signature to read.
-     *            A prior bounds check of this parameter should be performed, to avoid out of bounds
-     * access.
+     * @param signatures The signature format is a compact form of {bytes32 r}{bytes32 s}{uint8 v}
+     * Compact means uint8 is not padded to 32 bytes.
+     * @param pos Which signature to read. A prior bounds check of this parameter should be
+     * performed, to avoid out of bounds access.
      * @param signatures Concatenated {r, s, v} signatures.
      * @return v Recovery ID or Safe signature type.
      * @return r Output value r of the signature.
